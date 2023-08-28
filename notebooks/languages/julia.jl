e7cda53af237c3883ee341d842ee7f50e3d6f1ff
Stephan Sahm <stephan.sahm@jolin.io>
2023-08-28 07:28:59 +0000

    automatic backup

--- notebooks/languages/julia.jl
+++ notebooks/languages/julia.jl
@@ -88,6 +88,9 @@ md"""
 We can combine multiple input widgets together using markdown string and interpolation syntax `$`. E.g. let's bring everything into a table.
 """
 
+# ╔═╡ 0318f1f7-7d12-4dfd-9100-81e676237f6c
+
+
 # ╔═╡ e98574c7-8b09-47f9-b39f-544099c0d4db
 macro testref()
 	a = Ref{Any}(nothing)
@@ -1516,6 +1519,7 @@ version = "1.4.1+0"
 # ╠═084ed3a1-089d-4c5e-bad5-925e6fc73945
 # ╠═c5b2dfb4-4ccd-4f75-8bbb-8d690486b48b
 # ╟─9aaff669-66f8-46da-a42b-47968550e84b
+# ╠═0318f1f7-7d12-4dfd-9100-81e676237f6c
 # ╠═e98574c7-8b09-47f9-b39f-544099c0d4db
 # ╠═0f8c6eed-394f-4219-890b-a460942f3532
 # ╠═4eb4728b-afe8-4a9a-906f-40750f908226
