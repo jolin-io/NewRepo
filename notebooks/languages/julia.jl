ad19551f2b3323870a303964069c6a2c58a434d6
Stephan Sahm <stephan.sahm@jolin.io>
2023-08-28 08:51:09 +0000

    automatic backup

--- notebooks/languages/julia.jl
+++ notebooks/languages/julia.jl
@@ -137,6 +137,9 @@ set_c = Setter(1)
 # ╔═╡ d167928b-8a64-4f2a-97ad-33c1c3477d3d
 c = @get set_c
 
+# ╔═╡ e4cf0618-e9c6-407f-a8b6-ab40f84d87e8
+
+
 # ╔═╡ b40c757f-3348-4fae-9d57-f19c9d4fe34f
 myvar = @testeval()
 
@@ -1560,6 +1563,7 @@ version = "1.4.1+0"
 # ╠═d1e0486b-9bee-45b3-8386-8b15d51ab6a1
 # ╠═35ffd88e-7f51-4596-9efe-903f09ac2dc8
 # ╠═d167928b-8a64-4f2a-97ad-33c1c3477d3d
+# ╠═e4cf0618-e9c6-407f-a8b6-ab40f84d87e8
 # ╠═b40c757f-3348-4fae-9d57-f19c9d4fe34f
 # ╠═b42d1d6c-f99e-4471-8baa-d8380f0d47cd
 # ╠═74ab2315-a25a-492c-9735-f596185de530
