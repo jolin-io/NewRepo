b0ade32e43c358ac790a7b545db4983189d1a34a
Stephan Sahm <stephan.sahm@jolin.io>
2023-08-28 08:08:54 +0000

    automatic backup

--- notebooks/languages/julia.jl
+++ notebooks/languages/julia.jl
@@ -131,6 +131,9 @@ end
 macro testeval()
 	quote
 		@eval another_var = 4
+		1
+	end
+end
 
 # ╔═╡ 182f7b6c-cb26-41dc-ad6c-6c474a340231
 macro isolated_cell_ids()
@@ -1555,6 +1558,7 @@ version = "1.4.1+0"
 # ╠═01149b68-c48f-4b54-9e08-a521b32096e3
 # ╠═591b8313-15e5-497c-a92d-a4d249069808
 # ╠═d1e0486b-9bee-45b3-8386-8b15d51ab6a1
+# ╠═b40c757f-3348-4fae-9d57-f19c9d4fe34f
 # ╠═182f7b6c-cb26-41dc-ad6c-6c474a340231
 # ╠═ca13d2c2-f9e2-4595-bc78-2537238fa896
 # ╠═c7e4abfb-6813-4eb0-a31a-2b37e9438355
